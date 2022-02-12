6908ca59c232550115f3d5c3f400d97ac7b93ba0
Vladislav Kuschew <vladislav.kuschew@gmail.com>
2022-02-12 22:56:51 +0100

    install express-fileupload

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,11 @@ import express from 'express';
 const app = express();
 const PORT = 8000;
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
+app.post('/upload', function (req, res) {
+  console.log(req.files); // the uploaded file object
+});
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
